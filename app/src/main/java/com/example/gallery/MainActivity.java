7f480526d49d90a80a7ebeb2010e4550fedc8c8c
Dino <dino.devedzic2018@gmail.com>
2026-04-16 23:13:45 +0200

    Zadatak 1 (debug greske libl activity)

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.gallery;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.gallery.data.repository.ImageRepository;
+import com.example.gallery.ui.gallery.GalleryAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private GalleryAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        adapter = new GalleryAdapter();
+        adapter.setOnItemClickListener((item, position) ->
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show());
+        recyclerView.setAdapter(adapter);
+
+        adapter.setItems(ImageRepository.getInstance().getItemsList());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
