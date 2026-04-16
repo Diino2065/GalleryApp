d2dc025c6821919cd1970060bf1849138825ee57
Dino <dino.devedzic2018@gmail.com>
2026-04-16 22:53:14 +0200

    Zadatak 1 (korak 5: ImageItem model)

---
+++ app/src/main/java/com/example/gallery/data/model/imageItem.java
@@ -0,0 +1,37 @@
+package com.example.gallery.data.model;
+
+public class imageItem {
+    private final int id;
+    private final String title;
+    private final int imageRes;
+    private final String descriptionUrl;
+    private final String dateAdded;
+
+    public imageItem(int id, String title, int imageRes, String descriptionUrl, String dateAdded) {
+        this.id = id;
+        this.title = title;
+        this.imageRes = imageRes;
+        this.descriptionUrl = descriptionUrl;
+        this.dateAdded = dateAdded;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getImageRes() {
+        return imageRes;
+    }
+
+    public String getDescriptionUrl() {
+        return descriptionUrl;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+}
