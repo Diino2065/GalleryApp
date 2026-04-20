2f6c424f686c8b7c4c592df2bd03bb2984130d0f
Dino <dino.devedzic2018@gmail.com>
2026-04-20 21:41:54 +0200

    Zadatak 2 (3.5. WebActivity)

---
+++ app/src/main/java/com/example/gallery/ui/web/WebActivity.java
@@ -0,0 +1,71 @@
+package com.example.gallery.ui.web;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.gallery.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    public static final String EXTRA_URL = "extra_url";
+    public static final String EXTRA_TITLE = "extra_title";
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        webView = findViewById(R.id.webView);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra(EXTRA_URL);
+        String title = intent.getStringExtra(EXTRA_TITLE);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title != null ? title : getString(R.string.info));
+        }
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        if (url != null) {
+            webView.loadUrl(url);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
